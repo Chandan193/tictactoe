a7dd7e9fa0958271388f0a8549115928440c5063
Chandan193 <ghoshchandan1907@gmail.com>
2023-03-06 20:33:48 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () =>{
+  return(
+    <React.Fragment>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <small>This is Chandan!</small>
+    <br />
+    <b>This is bold</b>
+  </React.Fragment>
+  );
+};
+
+export default App;
