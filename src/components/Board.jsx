556c0490b483c96315f1bc2f5652f99fea019d1f
Chandan193 <ghoshchandan1907@gmail.com>
2023-03-26 14:25:23 +0530

    added board state and react events

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  // ------------Hook state in react-------------
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  // It will process the logic
+  const handleSquareClick = (position) => {
+
+    // If the square already then we will just return
+    if (board[position]) {
+      return;
+    }
+
+    setBoard((prev) => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {handleSquareClick(position)}}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-
-const Square = ({value, children}) => {
-
+const Square = ({ value, onClick }) => {
+  // We need to update the state when we click a square, here react evans comes into play
+  // By clicking "on" we can get a list on evans available for a html tag
   return (
-    <button type="button" className="square">{value}</button>)
-}
+    <button
+      type="button"
+      className="square"
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+};
 
-export default Square
-
+export default Square;
