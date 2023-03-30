2a67e087ab7c2911ed8caf20f8887735fe3e5484
Chandan193 <ghoshchandan1907@gmail.com>
2023-03-30 20:00:00 +0530

    added game history by changing the states

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,52 @@ import './styles/root.scss';
 
 const App = () => {
   // ------------Hook state in react-------------
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+
+  // It will be just an index inside the history array
+  const [currentMove, setCurrentMove] = useState(0);
+
   const [isXNext, setIsXNext] = useState(false);
 
-  const winner = calculateWinner(board);
+  const current = history[currentMove];
+
+  const winner = calculateWinner(current.board);
 
   const message = winner
     ? `Winner is ${winner}`
-    : `Next player is ${isXNext ? 'X' : 'O'}`;
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   // It will process the logic
   const handleSquareClick = position => {
     // If the square already then we will just return
-    if (board[position] || winner) {
+    if (current.board[position] || winner) {
       return;
     }
 
-    setBoard(prev => {
-      return prev.map((square, pos) => {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
-          return isXNext ? 'X' : 'O';
+          return last.isXNext ? 'X' : 'O';
         }
 
         return square;
       });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
 
-    setIsXNext(prev => !prev);
+    setCurrentMove(prev => prev + 1);
   };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
-      <Board board={board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
