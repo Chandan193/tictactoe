abb2d6efdb53d03024dd7f4e213b8a96506d5159
Chandan193 <ghoshchandan1907@gmail.com>
2023-04-01 12:09:53 +0530

    bolded the winning squares

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Square = ({ value, onClick, isWinningSquare }) => {
   // We need to update the state when we click a square, here react evans comes into play
   // By clicking "on" we can get a list on evans available for a html tag
-  
+
   // console.log('Square rerender');
-  
+
   return (
     <button
       type="button"
       className="square"
       onClick={onClick}
-      style={{fontWeight: isWinningSquare ? 'bold' : 'normal'}}
+      style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}
     >
       {value}
     </button>
